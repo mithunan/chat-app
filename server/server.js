5b07bf10a221be890a598544072c26f144e7da6c
Myth Raven <mithunan.r@gmail.com>
2019-01-08 13:55:16 -0500

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -14,12 +14,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected')
     
-    // socket.emit('newMessage', {
-    //     from: 'Mithunan',
-    //     text: "Is this working?",
-    //     createdAt: 123
-    // });
-
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the Chat App',
+        createdAt: new Date().getTime()
+    });
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New User Joined Chat',
+        createdAt: new Date().getTime()
+    });
+    
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
         io.emit('newMessage', {
@@ -27,6 +32,12 @@ io.on('connection', (socket) => {
             text: message.text,
             createdAt: new Date().getTime()
         })
+
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()  
+        // });
     });
 
     socket.on('disconnect', () => {

8c1ed57d03e0cf4aa3f37a0210d32c2e65ac8bd6
Myth Raven <mithunan.r@gmail.com>
2019-01-08 13:32:21 -0500

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,20 @@
+
+var socket = io();
+
+socket.on('connect', function() {
+    console.log('Connected to server');
+
+    socket.emit('createMessage', {
+        from: 'Myth',
+        text: 'Yup, that works for me'
+    })
+
+});
+
+socket.on('disconnect', function() {
+    console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function(message) {
+    console.log('New Message', message)
+});

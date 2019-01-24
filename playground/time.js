b28482e3e8cdaeaad94d1fb92d86fecaff19217a
Myth Raven <mithunan.r@gmail.com>
2019-01-24 14:34:17 -0500

    Add time stamp to messages

---
+++ playground/time.js
@@ -0,0 +1,13 @@
+var moment = require('moment');
+
+
+// var date = new Date();
+// var months = ['Jan', 'Feb']
+// console.log(date.getMonth());
+
+
+var date = moment();
+console.log(date.format('h:mm a'));
+
+
+

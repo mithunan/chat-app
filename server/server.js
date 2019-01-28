78e3c4b42f20f316b8f003ab36d734e7b6fe50c9
Myth Raven <mithunan.r@gmail.com>
2019-01-28 13:34:50 -0500

    Send messages to only people in same room

--- server/server.js
+++ server/server.js
@@ -49,8 +49,11 @@ io.on('connection', (socket) => {
     });
 
     socket.on('createMessage', (message, callback) => {
-        console.log('createMessage', message);
-        io.emit('newMessage', generateMessage(message.from,message.text));
+        var user = users.getUser(socket.id);
+        if (user && isRealString(message.text)) {
+            io.to(user.room).emit('newMessage', generateMessage(user.name,message.text));
+        }
+        
         callback();
         // socket.broadcast.emit('newMessage', {
         //     from: message.from,
@@ -61,9 +64,10 @@ io.on('connection', (socket) => {
 
 
     socket.on('createLocationMessage', (coords) => {
-        io.emit('newLocationMessage', generateLocationMessage('Oracle', coords.latitude, coords.longitude));
-        console.log(coords.latitude);
-        console.log(coords.longitude);
+        var user = users.getUser(socket.id);
+        if (user) {
+        io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+        }
     });
 
     socket.on('disconnect', () => {
